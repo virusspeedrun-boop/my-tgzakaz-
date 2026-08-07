ddaaa873fc100fb90af85b74329eb94ae00195ea
virusspeedrun-boop <virusspeedrun@gmail.com>
2026-08-07 23:27:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
+# Твой новый токен бота
 BOT_TOKEN = "8307051627:AAFnXTHGcZSn4Hyt08d8HVPf9poMeUIG8xE"
 
-allowed_raw = os.environ.get("ALLOWED_USERS", "8310775460")
-ALLOWED_USERS = [int(x.strip()) for x in allowed_raw.split(",") if x.strip().isdigit()]
+# Твой точный Telegram ID с правильной цифрой на конце
+ALLOWED_USERS = [8310775465]
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SESSIONS_DIR = os.path.join(BASE_DIR, "sessions")
