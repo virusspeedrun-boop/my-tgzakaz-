758d4bcc67b9ca8431bb12e8da0a334b0c5bb758
virusspeedrun-boop <virusspeedrun@gmail.com>
2026-08-09 01:56:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-import os
+\import os
 
 BOT_TOKEN = "8307051627:AAFnXTHGcZSn4Hyt08d8HVPf9poMeUIG8xE"
+
+# Твой ID и ID твоего друга без лишних пробелов и пустых мест
 ALLOWED_USERS = [8310775465, 8237762407]
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
