921534489d11a5fc1f47a1ef62b906d12a238ab1
virusspeedrun-boop <virusspeedrun@gmail.com>
2026-08-14 19:54:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 
 BOT_TOKEN = "8307051627:AAFnXTHGcZSn4Hyt08d8HVPf9poMeUIG8xE"
-
-# Твой ID и ID твоего друга без лишних пробелов и пустых мест
-ALLOWED_USERS = [8310775465, 8237762407]
+ALLOWED_USERS = [5116744917]  # Сюда впиши ID, если нужно расширить доступы
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SESSIONS_DIR = os.path.join(BASE_DIR, "sessions")
 
-os.makedirs(SESSIONS_DIR, exist_ok=True)
+if not os.path.exists(SESSIONS_DIR):
+    os.makedirs(SESSIONS_DIR, exist_ok=True)
